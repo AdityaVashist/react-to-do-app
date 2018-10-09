2f0593dad00e1867a36fffe89b1de2ec0337c921
avas10 <avashist3@sapient.com>
2018-10-09 14:57:37 +0530

    home route added

--- src/components/molecules/NavBar/NavBar.js
+++ src/components/molecules/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 
 const NavBar = () => <div>
     <ul>
+    <li><Link to="/">Home</Link></li>
         <li><Link to="/new">Add new task</Link></li>
         <li><Link to="/pending">Pending tasks</Link></li>
         <li><Link to="/completed">Completed tasks</Link></li>
